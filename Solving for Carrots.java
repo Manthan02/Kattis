68b0e3d6938c07e08d12ef2aab45470b78f6e068
Manthan02 <48860878+Manthan02@users.noreply.github.com>
2019-12-27 20:47:57 +0530

    Add files via upload

---
+++ Solving for Carrots.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class carrot
+{
+	public static void main(String arg[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int p=sc.nextInt();
+		System.out.println(p);
+	}
+}
